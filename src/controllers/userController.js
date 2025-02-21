635f5fc664cb13d5ed2af23eff8330b26ff8d031
kaynan-jamelaumn <kaynanrodrigues.nt@gmail.com>
2025-02-21 10:09:40 -0300

    Chore: migrated validate fields to IDValidator Class

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 import { logger } from "../../app.js";
 import path from 'path'
 import fs from "fs";
+import IDValidator from "../utils/IDValidator.js";
 
 dotenvExpand.expand(process.env);
 
@@ -24,7 +25,6 @@ try {
 
 class UserController {
   constructor() {
-    this.validateFields = this.validateFields.bind(this);
     this.checkFieldAvailability = this.checkFieldAvailability.bind(this);
     this.failedValidationAndDeletePhoto = this.failedValidationAndDeletePhoto.bind(this);
     this.create = this.create.bind(this);
@@ -49,7 +49,7 @@ class UserController {
       const profilePictureTruePath = profilePicture? path.join(uploadDir, req.file.filename) : null; // exemple /plublic/uploads/picture.png
       const { username, name, surname, email, password, bio, birthDate, role } = req.body;
   
-      this.failedValidationAndDeletePhoto(req, res,   this.validateFields({ username, name, surname, email, password, role }, { required: false }), profilePictureTruePath)
+      this.failedValidationAndDeletePhoto(req, res, IDValidator.validateFields({ username, name, surname, email, password, role }, { required: false }), profilePictureTruePath)
   
   
       const isEmailAvailable = await this.checkFieldAvailability(
@@ -215,7 +215,7 @@ class UserController {
       if (!user) {
         return res.status(404).json({ error: 'User not found.' });
       }
-      this.failedValidationAndDeletePhoto(req, res,   this.validateFields({ username, name, surname, email, password, role }, { required: false }), profilePictureTruePath)
+      this.failedValidationAndDeletePhoto(req, res,   IDValidator.validateFields({ username, name, surname, email, password, role }, { required: false }), profilePictureTruePath)
   
   
       const isEmailAvailable = await this.checkFieldAvailability(
@@ -513,54 +513,6 @@ class UserController {
     }
   }
 
-  validateFields(userData, options = { required: true }) {
-    const { username, name, surname, email, password, role } = userData;
-    const { required } = options;
-  
-    // Validate required fields
-    if (required && (!name || !surname || !email || !password || !username)) {
-      return { error: 'All fields (username, name, surname, email, password) are required.', status: 400 };
-    }
-  
-    // Validate name and surname length
-    if (name && name.length < 2) {
-      return { error: 'Name must be at least 2 characters long.', status: 400 };
-    }
-    if (surname && surname.length < 2) {
-      return { error: 'Surname must be at least 2 characters long.', status: 400 };
-    }
-    if (username && username.length < 2) {
-      return { error: 'Username must be at least 2 characters long.', status: 400 };
-    }
-  
-  
-    // Validate email format
-    if (email) {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(email)) {
-        return { error: 'Invalid email format.', status: 400 };
-      }
-    }
-  
-    // Validate password length and strength
-    if (password) {       //const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; <- password regex including special chars 
-      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
-      if (!passwordRegex.test(password)) {
-        return {
-          error:
-            'Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, and a number.',
-          status: 400,
-        };
-      }
-    }
-  
-    // Validate role
-    if (role && !['User', 'Admin'].includes(role)) {
-      return { error: 'Invalid role. Allowed values are "User" or "Admin".', status: 400 };
-    }
-  
-    return null; // No validation errors
-  }
 
   
 
--- src/utils/IDValidator.js
+++ src/utils/IDValidator.js
@@ -1,5 +1,54 @@
-import PassportValidator from "./passPortValidator";
+import PassportValidator from "./PassportValidator,js";
 class IDValidator {
-
+    static validateFields(userData, options = { required: true }) {
+        const { username, name, surname, email, password, role } = userData;
+        const { required } = options;
+      
+        // Validate required fields
+        if (required && (!name || !surname || !email || !password || !username)) {
+          return { error: 'All fields (username, name, surname, email, password) are required.', status: 400 };
+        }
+      
+        // Validate name and surname length
+        if (name && name.length < 2) {
+          return { error: 'Name must be at least 2 characters long.', status: 400 };
+        }
+        if (surname && surname.length < 2) {
+          return { error: 'Surname must be at least 2 characters long.', status: 400 };
+        }
+        if (username && username.length < 2) {
+          return { error: 'Username must be at least 2 characters long.', status: 400 };
+        }
+      
+      
+        // Validate email format
+        if (email) {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (!emailRegex.test(email)) {
+            return { error: 'Invalid email format.', status: 400 };
+          }
+        }
+      
+        // Validate password length and strength
+        if (password) {       //const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; <- password regex including special chars 
+          const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+          if (!passwordRegex.test(password)) {
+            return {
+              error:
+                'Password must be at least 8 characters long and include an uppercase letter, a lowercase letter, and a number.',
+              status: 400,
+            };
+          }
+        }
+      
+        // Validate role
+        if (role && !['User', 'Admin'].includes(role)) {
+          return { error: 'Invalid role. Allowed values are "User" or "Admin".', status: 400 };
+        }
+      
+        return null; // No validation errors
+      }
+    
  
   }
+  export default IDValidator;
